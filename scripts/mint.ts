8d3e945491d52c956d40ef51c8919a08ee22b38e
Carl <carl@CarldeMacBook-Air.local>
2025-09-10 20:01:06 +0800

    Carl_NFT

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -1,12 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const contractAddress = "0x90ccB4A90Fd5baD5389590331F2A6edD92BA1375"; // 你的合约地址
-  const recipients = [
+  const contractAddress = "0x99E7544f8516d9C672C58a1c8a8f4af0A65C4a4a"; // 你的合约地址
+  const recipients = ["0xB61D46B92EdF56DD605480f55dfC2EF27df08c36", "0x7c5F46c52A93b4Bf48C466faC5CF59f8b7A52300"
     //TODO：填写接收者地址
   ];
   //TODO：填写元数据链接（可以用这个“ipfs://bafkreiar3k5vy66vwe3xlmaoeeyypixtcfzyuxa4qcadpwqixvv4lapm3i”）
-  const tokenURI = ""; // 固定元数据链接
+  const tokenURI = "ipfs://bafkreibq3ngx7x7ys5u2rnphc7az7o5dm3pfvjwkwmsoaznywrdfz6d4xu"; // 固定元数据链接
 
   const MyNFT = await ethers.getContractAt("MyNFT", contractAddress);
 
